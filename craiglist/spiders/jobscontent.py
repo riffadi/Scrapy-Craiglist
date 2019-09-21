b6cbacc2cdd3debef021e2954f4341662a396622
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-09-22 00:36:01 +0700

    Scraping Craigslist's Engineering Job Details from Each Job's Web Page

---
+++ craiglist/spiders/jobscontent.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Request
+
+class MultiplePageSpider(scrapy.Spider):
+    name = 'jobscontent'
+    allowed_domains = ['craigslist.org']
+    start_urls = ['https://newyork.craigslist.org/search/egr/']
+
+    def parse(self, response):
+        jobs = response.xpath('//p[@class="result-info"]')
+        
+        for job in jobs:
+        	title = job.xpath('a/text()').extract_first()
+        	address = job.xpath('span[@class="result-meta"]/span[@class="result-hood"]/text()').extract_first("")[2:-1]
+        	urls = job.xpath('a/@href').extract_first()
+
+        	yield Request(urls, self.parse_page, meta={'URL' : urls, 'Title' : title, 'Address' : address})
+
+        relative_next_url = response.xpath('//a[@class="button next"]/@href').extract_first()
+        absolute_next_url = response.urljoin(relative_next_url)
+
+        yield Request(absolute_next_url, callback=self.parse)
+
+    def parse_page(self, response):
+    	url = response.meta.get('URL')
+    	title = response.meta.get('Title')
+    	address = response.meta.get('Address')
+
+    	description = "".join(line for line in response.xpath('//*[@id="postingbody"]/text()').extract()).strip()
+
+    	compensation = response.xpath('//p[@class="attrgroup"]/span[1]/b/text()').extract_first()
+    	employment_type  = response.xpath('//p[@class="attrgroup"]/span[2]/b/text()').extract_first()
+
+    	yield{'URL': url, 'Title': title, 'Address':address, 'Description':description, 'Compensation':compensation, 'Employment Type':employment_type}
